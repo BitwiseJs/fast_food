9a6916a218a55f6165e618497b02ca7cd0056ec4
BitwiseJs <mohfauzanasrori@gmail.com>
2025-07-05 22:20:48 +0700

    feat: add authentication screen

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
   endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
   platform: "com.bitwisejs.fastfood",
   projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  databaseId: "68692ab80020db8a7b1c",
+  userCollectionId: "68692aee00181f309191",
 };
